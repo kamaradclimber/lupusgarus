d8c9118ac7bd72979717c8ebe5d453b1f1594dc5
Grégoire Seux <kamaradclimber@users.noreply.github.com>
2008-07-20 13:37:13 +0000

    *maj au format wiki de la syntaxe des communications

--- syntaxe des communications (nest pas un ml) .ml
+++ syntaxe des communications (nest pas un ml) .ml
@@ -1,19 +1,31 @@
-(*il sagit dun .ml juste pour la coloration syntaxique*)
+(*il sagit dun .ml juste pour la coloration syntaxique, version a jour, syntaxée en format wiki*)
 
-les informations se font sous la forme dun couple (num de linfo,info sous la forme dun tableau)
-attention: le tableau ne doit contenir que des entiers (le type information est defini)
+#summary Résumé de la syntaxe des échanges d'information entre conteur et joueurs
+
+= Introduction =
+
+Vous trouverez une version plus à jour dans la source dans le ficher "syntaxe des communications (nest pas un ml) .ml"
+
+= Details =
+
+Les informations se font sous la forme dun couple (num de linfo,info sous la forme dun tableau)
+
+_attention_: le tableau ne doit contenir que des entiers (le type information est defini)
+
+||id info||syntaxe||signifie que: ||commentaire||
+||idi(-1) ||(-1,...) ||erreur ! ||(peut provenir de la reponse à une question inexistante)||
+||idi0 ||(0,[|nbjoueurs:int;id:int|])|| la partie réunit %nbjoueurs%, le joueur qui recoit cette info sera désigné par le numero %id%||
+||idi1 ||(1,[|untel:int;personnalité:int|]) || %untel% est de telle %personnalité% || (NB utiliser la fonction num2perso)||
+
+les questions seront sous la forme dun couple egalement: (numero de la question, information eventuelement necessaire sous la forme dun tableau)
 
 
-idi(-1) (-1,_) erreur ! (peut provenir de la reponse à une question iniexistante)
-idi0 (0,[|nbjoueurs:int;id:int|]) signifie que la partie réunit %nbjoueurs%, le joueur qui recoit cette info sera désigné par le numero %id%
-idi1 (1,[|untel:int;personnalité:int|]) signifie %untel% est de telle %personnalité% (NB utiliser la fonction num2perso)
+  * les question portent des numeros qui essayent de correspondre à une info du meme genre donnée par le conteur
+  * la reponse doit se faire sous la forme dun type information (num de l'info, info sous la forme dun tableau)
 
-les questions seront sous la forme dun couple egalement: (numero de la question* information eventuelement necessaire sous la forme dun tableau)
--les question portent des numeros qui essayent de correspondre à une info du meme genre donnée par le conteur
--la reponse doit se faire sous la forme dun type information (num de l'info, info sous la forme dun tableau)
-
-idq0 (0,[||]) signifie "donne moi nb joueur & ton numero", reponse: idi0
-idq1 (1,[|untel:int|]) signifie "donne moi lidentité de untel selon toi", reponse: idi1
+|| id de la question||syntaxe||signifie:||réponse appropriée:||
+||idq0 ||(0,[| |])|| "donne moi nb joueur & ton numero"|| idi0||
+||idq1 ||(1,[|untel:int|])|| "donne moi lidentité de untel selon toi"|| idi1||
 
 
 
